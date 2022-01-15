018c83e123804afd91988eb3a8939fa326d780d9
Mateus Pavao <mateus_e_mateus@hotmail.com>
2022-01-15 19:09:10 +0100

    changing layout

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ function Header() {
     return (
         <div className="main-header">
             <h1>QUIZ GAME APP</h1>
-            <h1>Test your knowlogde below!</h1>
+            <h1>Test your knowlegde below!</h1>
         </div>
     )
 }
